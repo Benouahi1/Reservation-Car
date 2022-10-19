0ce2dc6bf844b5dbc2941e291737283d445d6820
ayoub benouahi <abenouahi5@gmail.com>
2022-10-19 16:32:07 +0100

    Diagramme de class

--- Controller/Voyages.js
+++ Controller/Voyages.js
@@ -18,24 +18,32 @@ res.json(Voyer);
   }
 });
 
-router.get('/:Ville',async (req, res)=>{
-  const Voyage = await  Voyages.find({_id:req.params.VoyageId})
-  res.json(Voyage[0].Circulation[2]);
-  const circl = Voyage[0].Circulation; 
-  const h = Voyage[0].Hours_Circulation;
-  const Prix = Voyage[0].Prix;
-  for(let i = 0; i < circl.length; i++){
-    if(i == 0){
-      console.log(circl[i]+ ' ' + h[i])
-    }else{
-      console.log(circl[i]+ ' ' + h[i]+' '+Prix[i]+'.00 Dh'); 
-    }
+router.get('/Cherche',async (req, res)=>{
+  const Voyage = await  Voyages.find()
+  
+  for (let i = 0; i < Voyage.length; i++) {
+   let Num = Voyage[i].Circulation.length;
+   for (let j = 0; j < Num-1; j++) {
+    
+    console.log(Voyage[i].Circulation[j] +" "+ Voyage[i].Hours_Circulation[j] +" h");
+   }
+    
+  }
+  
+  // const circl = Voyage[0].Circulation; 
+  // const h = Voyage[0].Hours_Circulation;
+  // const Prix = Voyage[0].Prix;
+  // for(let i = 0; i < circl.length; i++){
+  //   if(i == 0){
+  //     console.log(circl[i]+ ' ' + h[i])
+  //   }else{
+  //     console.log(circl[i]+ ' ' + h[i]+' '+Prix[i]+'.00 Dh'); 
+  //   }
    
   
-  }
+  // }
  try{
-     const savedCar = await care.save();
-     res.json(savedCar);
+  res.json(Voyage);
  }catch(err){
      res.json({message: err});
  }
@@ -70,9 +78,7 @@ router.post('/Add', async (req, res)=>{
  const Voyer = new Voyages({
     Name_Car: req.body.Name_Car,
     Numero_Car: req.body.Numero_Car,
-    
     Date_depart: req.body.Date_depart,
-   
     Circulation: req.body.Circulation,
     Hours_Circulation: req.body.Hours_Circulation,
     Prix: req.body.Prix,
--- app.js
+++ app.js
@@ -24,7 +24,19 @@ app.use('/Car',carRoute);
 app.use('/User',UserRoute);
 app.use('/Admin',AdminRoute);
 
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*"); 
+//     res.header(
+//       "Access-Control-Allow-Headers",
+//       "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//     );
+//   if (req.method == "OPTIONS") {
+//     res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+//     return res.status(200).json({});
+//   }
 
+//   next();
+//   })
 
 app.get('/', (req, res) =>{
 
